b4227270aef598d40083c8b87a5f3a0d020f1846
Anujsri <anujsrivastava@iiitdmj.ac.in>
2018-12-19 00:23:35 +0530

    new code

--- client/main.js
+++ client/main.js
@@ -1,6 +1,10 @@
+BlazeLayout.setRoot('body');
 import { Template } from 'meteor/templating';
 import { Notes } from '../lib/collections.js'; 
+import { NotesIndex } from '../lib/collections.js'; 
 import { Accounts } from 'meteor/accounts-base';
+import {Router}      from '../lib/routes.js';
+import { Session } from 'meteor/session'
 
 //Accounts config
 
@@ -8,31 +12,34 @@ Accounts.ui.config({
   	passwordSignupFields: 'USERNAME_ONLY',
 });
 
-import './main.html';
+import './layouts/HomeLayout.html'
+import './layouts/BasicLayout.html'
+// import './main.html';
 
-Template.body.helpers({
-   // 	notes : [
-   // 		{text : 'My Note 1'},
-   // 		{text : 'My Note 2'},
-   // 		{text : 'My Note 3'},
-   // ]
-   notes(){
+Meteor.subscribe("notes");
+
+Template.allnotesa.helpers({
+    notes(){
+   		return Notes.find({});
+    }
+});
+
+Template.AllNotes.helpers({
+	notesa : ()=>{
    		return Notes.find({});
    }
 });
 
 Template.add.events({
 	'click .add-form' :function(){
-		console.log("ANuj");
 		event.preventDefault();
 
 		//Get input Value
 
 		const target = event.target;
+
 		const text = document.getElementById('name').value;
-		console.log("ANuj : "+ text);
 		 const authorname = document.getElementById('authorname').value;
-		console.log("Name : " + authorname)
 
 		//Insert note into collection
 		// var authorname = "Anuj";
@@ -57,8 +64,41 @@ Template.note.events({
 	}
 })
 
- 
+Template.HomeLayout.events({
+    'submit .search_note' : function(){
+        event.preventDefault();
+        const target = event.target;
+        var text = target.text.value;
+        console.log("Text : " + text);
+        // On Client
+        var result  = [];
+        var res = document.getElementById('searchednote');
+        Tracker.autorun(function () {
+          let cursor = NotesIndex.search(text) ;
+          var len =  cursor.count();
+          if(len===0){
+              console.log("Not Found");
+              Session.set('notnotes', 'No data found with keyword "' + text +'"');
+          }
+          else{
+              result = cursor.fetch();
+          } 
+        })
+        res.innerHTML = 'Searched Keyword "' + text + '"';
+        target.text.value="";
+        Session.set('notes', result);
+        return false;
+    }
+});
 
- 
+Template.Serachbox.helpers({
+    result : ()=>{
+        return Session.get('notes');
+    }
+})
 
- 
+Template.Serachbox.helpers({
+     notresult : ()=>{
+        return Session.get('notnotes');
+    }
+})
--- server/main.js
+++ server/main.js
@@ -1,7 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 // export const Notes = new Mongo.Collection('notes'); 
-import '../lib/collections.js';
+import { Notes } from '../lib/collections.js';
 Meteor.startup(() => {
   // code to run on server at startup
+});
+
+Meteor.publish("notes",function(){
+	return Notes.find();
 });
